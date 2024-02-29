899463783f60b9872874639acce180bf1d7a604a
Valejandro08 <143618679+Valejandro08@users.noreply.github.com>
2024-02-29 15:39:31 +0000

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+let introvert= document.querySelector(".introvert")
+let extrovert= document.querySelector(".extovert")
+let yes= document.querySelector(".yes")
+let no= document.querySelector(".no")
+
+introvert.addEventListener("click")
