1a73b90249c6041b2cfebcaf81c37b1c9b856a30
Valejandro08 <143618679+Valejandro08@users.noreply.github.com>
2024-03-11 14:38:54 +0000

    new

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-let introvert= document.querySelector(".introvert")
-let extrovert= document.querySelector(".extovert")
-let yes= document.querySelector(".yes")
-let no= document.querySelector(".no")
+let introvert= document.querySelector(".introvert");
+let extrovert= document.querySelector(".extrovert");
+let yes= document.querySelector(".yes");
+let no= document.querySelector(".no");
 
-introvert.addEventListener("click")
+introvert.addEventListener("click", function(){ 
+
+});
+
+
+extrovert.addEventListener("click", function(){ 
+
+});
+
+yes.addEventListener("click", function(){
+
+});
+
+
+no.addEventListener("click", function(){
+
+});
+let choice1= document.querySelector(".choice1");
+let choice2= document.querySelector(".choice2");
+let message= document.querySelector(".message");
+if (choice1==="introvert" && choice2=== "no") {message.innerHTML="you are a Cane Corso";}
