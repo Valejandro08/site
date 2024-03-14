9ad28c2e26d7e71a9390d1f48227f721dbd5b939
Valejandro08 <143618679+Valejandro08@users.noreply.github.com>
2024-03-14 14:40:13 +0000

    new

--- script.js
+++ script.js
@@ -2,25 +2,48 @@ let introvert= document.querySelector(".introvert");
 let extrovert= document.querySelector(".extrovert");
 let yes= document.querySelector(".yes");
 let no= document.querySelector(".no");
+let submit= document.querySelector(".submit");
+let input= document.querySelector(".choice1");
+let input2= document.querySelector(".choice2");
 
-introvert.addEventListener("click", function(){ 
+introvert.addEventListener("click", function(){
+    input=introvert;
+    
+    });
+    
+    
+    
+    
+    extrovert.addEventListener("click", function(){
+    input=extrovert;
+  
+    });
+    
+    
+    yes.addEventListener("click", function(){
+    input2= yes;
+   
+    });
+    
+    
+    
+    
+    no.addEventListener("click", function(){
+        input2= no;
+       
+    });
+    
 
+
+
+submit.addEventListener("click", function(){
+    let message= document.querySelector(".message");  
+if (input===introvert && input2=== no) {message.innerHTML="you are a Cane Corso";}
+
+else if (input=== introvert && input2===yes) {message.innerHTML="you are a Corgi";}
+
+else if (input===extrovert && input2===no) {message.innerHTML="you are a Pitbull";}
+
+else if (input===extrovert && input2===yes) {message.innerHTML="you are a Golden Retriever";}
 });
 
-
-extrovert.addEventListener("click", function(){ 
-
-});
-
-yes.addEventListener("click", function(){
-
-});
-
-
-no.addEventListener("click", function(){
-
-});
-let choice1= document.querySelector(".choice1");
-let choice2= document.querySelector(".choice2");
-let message= document.querySelector(".message");
-if (choice1==="introvert" && choice2=== "no") {message.innerHTML="you are a Cane Corso";}
